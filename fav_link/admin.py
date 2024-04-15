b77c0d655d86554b9584bab3342760a42a559fd8
Nachai <56039177+NaPryder@users.noreply.github.com>
2024-04-15 14:46:45 +0700

    add fav link admin

--- fav_link/admin.py
+++ fav_link/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from fav_link.models import Tag, Category, FavoriteUrl
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(FavoriteUrl)
