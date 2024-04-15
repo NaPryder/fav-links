fa4a4edf50008bf1c966c003aba6ec8a9d007552
Nachai <56039177+NaPryder@users.noreply.github.com>
2024-04-16 01:58:11 +0700

    add queues as global variable

--- fav_link/apps.py
+++ fav_link/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
+UPDATING_QUEUES = []
+
 
 class FavLinkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fav_link'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fav_link"
