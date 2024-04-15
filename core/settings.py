67aac67dc27cd30abd6e97cfdc39e26b7b6b0c24
Nachai <56039177+NaPryder@users.noreply.github.com>
2024-04-15 21:33:09 +0700

    add  django filter

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "account",
     "fav_link",
 ]
@@ -99,6 +100,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 # Database
